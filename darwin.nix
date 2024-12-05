c40d920dd8849c80cace6d5bacd2465eee05d002
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2024-12-05 19:08:46 +0100

    feat: separate pkgs source to downgrade bitwarden-cli (#5)
    * feat: separate pkgs source to downgrade bitwarden-cli
    
    * remove brewed version

--- darwin.nix
+++ darwin.nix
@@ -1,4 +1,4 @@
-{ pkgs, userConfig, ... }: 
+{ pkgs, pkgs-bwcli, userConfig, ... }: 
 
 {
   services.nix-daemon.enable = true;
@@ -71,6 +71,7 @@
 
   environment.systemPackages = [
     pkgs.emacs29
+    pkgs-bwcli.bitwarden-cli
   ];
 
   fonts.packages = with pkgs; [
@@ -83,9 +84,6 @@
     onActivation.cleanup = "uninstall";
 
     taps = [];
-    brews = [
-      "bitwarden-cli"
-    ];
     casks = [
       "firefox"
     ];
