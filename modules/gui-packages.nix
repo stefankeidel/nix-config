9102b551e64f8ca3c45b459b27145e3167697505
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2024-12-29 22:01:19 +0100

    +appcleaner

--- modules/gui-packages.nix
+++ modules/gui-packages.nix
@@ -4,11 +4,12 @@
   ...
 }:
 with pkgs; [
+  pkgs.appcleaner
+  pkgs.dockutil
   pkgs.jetbrains.datagrip
   pkgs.raycast
   pkgs.signal-desktop
   pkgs.spotify
   pkgs.wezterm
-  pkgs.dockutil
   pkgs.zoom-us
 ]
