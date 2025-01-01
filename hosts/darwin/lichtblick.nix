f4c6f16ea542157e288cbdce78b62c4ca4e533b6
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-01-01 09:40:45 +0100

    start backing up and also remove dock stuff

--- hosts/darwin/lichtblick.nix
+++ hosts/darwin/lichtblick.nix
@@ -12,13 +12,18 @@
     emacsclient -c -n 1>/dev/null 2>&1 &
   '';
 in {
-  imports = [
-    ../../modules/dock
-  ];
+  # imports = [
+  #   ../../modules/dock
+  # ];
 
   home-manager.users.${userConfig.name}.home.packages = with pkgs; [
     pkgs.azure-cli
     pkgs.kubelogin
+    (writeShellScriptBin "do_bak" ''
+      #!/usr/bin/env zsh
+      set -e
+      restic --repo rclone:sb:lichtblick-bak backup ~/code ~/Documents ~/Desktop ~/Nextcloud --skip-if-unchanged
+    '')
   ];
 
   # homebrew = {
@@ -33,24 +38,24 @@ in {
   # };
 
   # Fully declarative dock using the latest from Nix Store
-  local.dock.enable = true;
-  local.dock.entries = [
-    {path = "${userConfig.home}/Applications/Home Manager Apps/WezTerm.app/";}
-    {path = "/Applications/Firefox.app/";}
-    {path = "/System/Applications/Calendar.app/";}
-    {path = "/Applications/Microsoft Outlook.app/";}
-    {path = "/Applications/Microsoft Teams.app/";}
-    {path = "${pkgs.emacs29}/bin/emacs-29.4";}
-    {path = "/System/Applications/Mail.app/";}
-    {path = "${userConfig.home}/Applications/Home Manager Apps/DataGrip.app/";}
-    {path = "/System/Applications/Notes.app/";}
-    {path = "${userConfig.home}/Applications/Home Manager Apps/Signal.app/";}
-    {path = "${userConfig.home}/Applications/Home Manager Apps/Spotify.app/";}
-    {path = "/System/Applications/System Settings.app/";}
-    {path = "/System/Applications/Photos.app/";}
-    {
-      path = toString myEmacsLauncher;
-      section = "others";
-    }
-  ];
+  # local.dock.enable = true;
+  # local.dock.entries = [
+  #   {path = "${userConfig.home}/Applications/Home Manager Apps/WezTerm.app/";}
+  #   {path = "/Applications/Firefox.app/";}
+  #   {path = "/System/Applications/Calendar.app/";}
+  #   {path = "/Applications/Microsoft Outlook.app/";}
+  #   {path = "/Applications/Microsoft Teams.app/";}
+  #   {path = "${pkgs.emacs29}/bin/emacs-29.4";}
+  #   {path = "/System/Applications/Mail.app/";}
+  #   {path = "${userConfig.home}/Applications/Home Manager Apps/DataGrip.app/";}
+  #   {path = "/System/Applications/Notes.app/";}
+  #   {path = "${userConfig.home}/Applications/Home Manager Apps/Signal.app/";}
+  #   {path = "${userConfig.home}/Applications/Home Manager Apps/Spotify.app/";}
+  #   {path = "/System/Applications/System Settings.app/";}
+  #   {path = "/System/Applications/Photos.app/";}
+  #   {
+  #     path = toString myEmacsLauncher;
+  #     section = "others";
+  #   }
+  # ];
 }
