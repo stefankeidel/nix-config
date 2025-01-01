6f600a1711dfd164493bec09c420bee42494a4b2
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-01-01 18:15:25 +0100

    disable declarative dock for mini

--- hosts/darwin/mini.nix
+++ hosts/darwin/mini.nix
@@ -12,9 +12,9 @@
     emacsclient -c -n 1>/dev/null 2>&1 &
   '';
 in {
-  imports = [
-    ../../modules/dock
-  ];
+  # imports = [
+  #   ../../modules/dock
+  # ];
 
   home-manager.users.${userConfig.name}.home.packages = with pkgs; [
     pkgs.zoom-us
@@ -30,24 +30,24 @@ in {
   ];
 
   # Fully declarative dock using the latest from Nix Store
-  local.dock.enable = true;
-  local.dock.entries = [
-    {path = "${userConfig.home}/Applications/Home Manager Apps/WezTerm.app/";}
-    {path = "/Applications/Firefox.app/";}
-    {path = "/System/Applications/Calendar.app/";}
-    {path = "${pkgs.emacs29}/bin/emacs-29.4";}
-    {path = "/System/Applications/Mail.app/";}
-    {path = "${userConfig.home}/Applications/Home Manager Apps/DataGrip.app/";}
-    {path = "/System/Applications/Notes.app/";}
-    {path = "${userConfig.home}/Applications/Home Manager Apps/Signal.app/";}
-    {path = "${userConfig.home}/Applications/Home Manager Apps/Spotify.app/";}
-    {path = "/Applications/Steam.app";}
-    {path = "${userConfig.home}/Applications/Home Manager Apps/Discord.app/";}
-    {path = "/System/Applications/System Settings.app/";}
-    {path = "/System/Applications/Photos.app/";}
-    {
-      path = toString myEmacsLauncher;
-      section = "others";
-    }
-  ];
+  # local.dock.enable = true;
+  # local.dock.entries = [
+  #   {path = "${userConfig.home}/Applications/Home Manager Apps/WezTerm.app/";}
+  #   {path = "/Applications/Firefox.app/";}
+  #   {path = "/System/Applications/Calendar.app/";}
+  #   {path = "${pkgs.emacs29}/bin/emacs-29.4";}
+  #   {path = "/System/Applications/Mail.app/";}
+  #   {path = "${userConfig.home}/Applications/Home Manager Apps/DataGrip.app/";}
+  #   {path = "/System/Applications/Notes.app/";}
+  #   {path = "${userConfig.home}/Applications/Home Manager Apps/Signal.app/";}
+  #   {path = "${userConfig.home}/Applications/Home Manager Apps/Spotify.app/";}
+  #   {path = "/Applications/Steam.app";}
+  #   {path = "${userConfig.home}/Applications/Home Manager Apps/Discord.app/";}
+  #   {path = "/System/Applications/System Settings.app/";}
+  #   {path = "/System/Applications/Photos.app/";}
+  #   {
+  #     path = toString myEmacsLauncher;
+  #     section = "others";
+  #   }
+  # ];
 }
