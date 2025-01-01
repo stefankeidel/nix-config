de370d99a2145f78a713ce91b0932980554d7195
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-01-01 13:30:57 +0100

    prune backup as well

--- hosts/darwin/lichtblick.nix
+++ hosts/darwin/lichtblick.nix
@@ -23,6 +23,7 @@ in {
       #!/usr/bin/env zsh
       set -e
       restic --repo rclone:sb:lichtblick-bak backup ~/code ~/Documents ~/Desktop ~/Nextcloud --skip-if-unchanged
+      restic --repo rclone:sb:lichtblick-bak forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --keep-yearly 1 --prune
     '')
   ];
 
