d079506e7af00c5a6d866861fdd9679148e18b64
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-01-13 09:49:32 +0100

    oops. zsh is not whitespace agnostic eh

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -96,9 +96,9 @@ in {
 
             eval "$(/opt/homebrew/bin/brew shellenv)"
 
-            export XDG_DATA_HOME = $HOME/.local/share
-            export XDG_STATE_HOME = $HOME/.local/state
-            export XDG_CACHE_HOME = $HOME/.cache
+            export XDG_DATA_HOME=$HOME/.local/share
+            export XDG_STATE_HOME=$HOME/.local/state
+            export XDG_CACHE_HOME=$HOME/.cache
 
             source ~/.functions
             source ~/.extra
