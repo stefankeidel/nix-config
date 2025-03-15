15e40b5258e24b7f122e1291323f05885b9892d3
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-03-15 22:45:59 +0100

    nvm

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -54,8 +54,8 @@
   # packages to install on Darwin desktop systems
   # headless (default) packages get pulled in by the home manager module
   home-manager.users.${userConfig.name}.home.packages = lib.mkMerge [
-    # (pkgs.callPackage ../../modules/dev-packages.nix {inherit inputs;})
-    (pkgs.callPackage ../../modules/gui-packages.nix {inherit inputs;})
+    (pkgs.callPackage ../../modules/dev-packages.nix {inherit inputs;})
+    # (pkgs.callPackage ../../modules/gui-packages.nix {inherit inputs;})
   ];
 
   # Necessary for using flakes on this system.
