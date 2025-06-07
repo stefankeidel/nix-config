027aaac930984fb22b9e0023f6f16c8ba9a81833
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-06-07 23:55:01 +0200

    fuck me, I think I've been doing backup all wrong

--- hosts/nixie/backup.nix
+++ hosts/nixie/backup.nix
@@ -20,11 +20,11 @@
                 --create-options \
                 nextcloud > /var/lib/nextcloud/db_$d.sql
 
-      /run/current-system/sw/bin/restic --password-file /run/agenix/restic --repo /var/bak/restic backup /var/lib/nextcloud --skip-if-unchanged
-
       /run/current-system/sw/bin/nextcloud-occ maintenance:mode --off
 
       /run/current-system/sw/bin/pg_dump -U postgres -d accounting -f /var/lib/nextcloud/accounting.sql
+
+      /run/current-system/sw/bin/restic --password-file /run/agenix/restic --repo /var/bak/restic backup /var/lib/nextcloud --skip-if-unchanged
 
       /run/current-system/sw/bin/restic --password-file /run/agenix/restic --repo /var/bak/restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 1 --prune
 
